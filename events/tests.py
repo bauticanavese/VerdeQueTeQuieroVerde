f4a5e663fafa9f0d5d7efc291beeb54349dac998
guille <guillecondori19@gmail.com>
2020-06-07 20:00:27 -0300

    added simple test for model

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Event
+
+class EventModel(TestCase):
+    def test_name(self):
+        event = Event(title = 'First event')
+        self.assertIs(event.title, 'First event')
