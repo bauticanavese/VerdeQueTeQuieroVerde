f1611d28046d3b7995cc7ff7e819d32d9bf7d0c4
edjus <ejusto@fi.uba.ar>
2020-06-12 02:24:28 -0300

    Add base template and home page

--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Bienvenido a la pagina de eventos")
+    my_dict = {'saludo':"Hello from events view!!!"}
+    return render(request, 'events/index.html', context=my_dict)
--- verdequetequieroverde/settings.py
+++ verdequetequieroverde/settings.py
@@ -20,6 +20,7 @@ if os.environ['ENVIRONMENT'] == 'production':
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'verdequetequieroverde.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
