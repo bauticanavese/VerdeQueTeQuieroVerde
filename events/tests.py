5dbfda76aa1fc8257141c2a3ffdf0bcbd4a68a5a
guille <guillecondori19@gmail.com>
2020-06-14 16:02:52 -0300

    added event fields and migrations for db

--- events/tests.py
+++ events/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils import timezone
 
 # Create your tests here.
 from .models import Event
@@ -7,3 +8,11 @@ class EventModel(TestCase):
     def test_name(self):
         event = Event(title = 'First event')
         self.assertIs(event.title, 'First event')
+
+    def test_default_fields(self):
+        creation_date = timezone.now
+        event = Event(title = 'Event')
+        self.assertIs(event.capacity, 0)
+        self.assertIs(event.title, 'Event')
+        self.assertEqual(event.place, 'event place')
+        self.assertEqual(event.topic, 'event topic')
