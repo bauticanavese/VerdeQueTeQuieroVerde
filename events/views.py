06dc651749da1cfe6b9ea452814049feecb09ad0
guille <guillecondori19@gmail.com>
2020-06-14 20:34:08 -0300

    view of first five events

--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Event
 
 def index(request):
-    my_dict = {'saludo':"Hello from events view!!!"}
-    return render(request, 'events/index.html', context=my_dict)
+    events = Event.objects.all()[:5]
+    context = { 'events_list' : events }
+    return render(request, 'events/index.html', context=context)
