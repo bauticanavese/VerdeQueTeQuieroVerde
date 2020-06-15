b94ea110606f5ab3658c4a79df10a5cdf7549cb7
guille <guillecondori19@gmail.com>
2020-06-15 17:27:42 -0300

    pagination for events

--- events/views.py
+++ events/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import Event
 
 def index(request):
-    events = Event.objects.all()[:5]
-    context = { 'events_list' : events }
+    post_per_page = 5
+    try:
+        page_number = int(request.GET.get('page'))
+        index = page_number*post_per_page - 1
+    except:
+        index = 0
+    events = Event.objects.all()[index:index+post_per_page]
+    visited = index * post_per_page + post_per_page
+    has_more = True if(Event.objects.all().count() > visited) else False
+    context = { 'events_list' : events , 'has_more' : has_more }
     return render(request, 'events/index.html', context=context)
