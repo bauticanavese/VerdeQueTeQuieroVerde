3f798fee7bc5b6b1c99f1af3a580dff275f309f5
edjus <ejusto@fi.uba.ar>
2020-06-27 20:00:34 -0300

    Add validations for trivias: dni required, play once a day

--- scores/views.py
+++ scores/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from urllib.parse import urlencode
+from datetime import date
 
 from .models import Score
 from trivias.models import Choice
@@ -11,20 +13,33 @@ def calculate_score(choices_ids):
   return sum(c.is_correct for c in choices)*SCORE_PER_QUESTION
 
 def create_score(participant, dni, total_score):
+  past_scores = Score.objects.filter(dni=dni, play_date__gte=date.today())
+  if len(past_scores) > 0:
+    raise Exception("Ya participó en la trivia")
+
   score = Score(participant=participant, dni=dni, score=total_score)
   score.save()
   return score
 
 def score(request):
   if request.method == 'POST':
-    participant = request.POST.get('name', '')
-    dni = int(request.POST.get('dni', ''))
-    selected_choices = { int(v) for (k,v) in request.POST.items() if k.isnumeric()}
-    total_score = calculate_score(selected_choices)
-    score = create_score(participant, dni, total_score)
+    try:
+      participant = request.POST.get('name', '')
+      dni = request.POST.get('dni', '')
+      if not dni:
+        raise Exception("Debe ingresar su dni")
+      dni = int(dni)
+      selected_choices = { int(v) for (k,v) in request.POST.items() if k.isnumeric()}
+      total_score = calculate_score(selected_choices)
+      score = create_score(participant, dni, total_score)
+    except Exception as e:
+      query_string =  urlencode({'error_message': e.args[0]})
+      url = '/trivias?{}'.format(query_string)
+      response = redirect(url)
+      return response
     
     context = {'participant': score.participant, 'dni': score.dni, 'score': score.score, 'date': score.play_date}
 
     return render(request, 'scores/score.html', context=context)
-  else:
-    return HttpResponse()
+  else: 
+      return redirect('/')
--- trivias/views.py
+++ trivias/views.py
@@ -4,7 +4,9 @@ from .models import Trivia
 from .models import Choice
 
 def index(request):
+  error_message = request.GET.get('error_message', '')
   trivias = Trivia.objects.all()
   choices = Choice.objects.all()
-  context = {'trivias': trivias, 'choices': choices}
+  context = {'trivias': trivias, 'choices': choices, 'error': error_message}
+
   return render(request, 'trivias/index.html', context=context)
